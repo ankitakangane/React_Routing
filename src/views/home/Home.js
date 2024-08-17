57670a0515f589a25b8ca49d2d25c5895234d9c4
ankitakangane <kanganeankita1@gmail.com>
2024-08-18 05:17:39 +0530

    routing

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from "../../components/navbar/Navbar";
 import home_img from "./home.jpg (2).png";
 import Footer from "../../components/footer/Footer";
+import toast,{Toaster} from "react-hot-toast";
 
 
 function Home() {
@@ -10,7 +11,28 @@ function Home() {
         <Navbar/>
       <h1 className="product_title">Home</h1>
       <img src={home_img} className="product_img"/>
+      <div className="butn_container">
+      
+      <button type="button" className="butn" onClick = { () => {
+        toast.loading("Please wait !!!");
+      }}>
+        Load
+      </button>
+
+      <button type="button" className="butn" onClick = { () => {
+        toast.success("Loaded Successfully...");
+      }}>
+        Finish
+      </button>
+
+      <button type="button" className="butn" onClick = { () => {
+        toast.error("Failed to load");
+      }}>
+        Fail
+      </button>
+      </div>
       <Footer/>
+      <Toaster/>
     </div>
   )
 }
